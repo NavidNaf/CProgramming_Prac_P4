48dabcf9965bc3c741439bf911ef17825a5da385
NavidNaf <33064619+NavidNaf@users.noreply.github.com>
2020-06-04 00:23:27 +0600

    Copying from one to another

--- copy.c
+++ copy.c
@@ -4,13 +4,13 @@
 #include <ctype.h>
 #include <cs50.h>
 
-int main (void)
+int main(void)
 {
     char *s = get_string("Name: ");
     char *t = malloc(strlen(s) + 1); //byte allocation
 
     //copying from s_memory to t_memory
-    strcpy(t,s);
+    strcpy(t, s);
 
     //for (int i = 0, n = strlen(s); i < n + 1; i++)
     //{
