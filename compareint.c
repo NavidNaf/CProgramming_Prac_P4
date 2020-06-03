f8f1717b5b7785d27162ab20e82a8832aec6d2fa
NavidNaf <33064619+NavidNaf@users.noreply.github.com>
2020-06-03 23:20:58 +0600

    Integer Compare using Pointer

---
+++ compareint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+
+    printf("i: ");
+    scanf("%i", &i);
+    printf("j: ");
+    scanf("%i", &j);
+
+    // pointer declare
+    int *i1 = &i;
+    int *j1 = &j;
+
+    if (i == j)
+        printf("Same\n");
+    else
+        printf("Different\n");
+
+    //compare using pointer
+    if (*i1 == *j1)
+        printf("Same\n");
+    else
+        printf("Different\n");
+
+}
